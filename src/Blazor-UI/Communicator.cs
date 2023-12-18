b7343ec847b443dd6a042fe1d6bee3b23f10f216
Twinki <thetwinkehh@gmail.com>
2023-12-17 22:57:47 -0500

    init (for real this time dog)

---
+++ src/Blazor-UI/Communicator.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using Microsoft.JSInterop;
+using RestSharp;
+
+namespace BlazorClient;
+
+public static class Communicator
+{
+    public static string AppStyle = "display: none";
+
+    private static Dictionary<string, List<Action<string>>> _events = new();
+    private static string ParentResourceName { get; set; }
+
+    public static string TriggerNuiCallback(string name, dynamic data)
+    {
+        string json = JsonSerializer.Serialize(data);
+        Console.WriteLine($"Triggering nui callback {name}: {json}");
+
+        var client = new RestClient($"https://{ParentResourceName}");
+
+        var request = new RestRequest($"/{name}", Method.Post);
+        request.AddParameter("application/json", json, ParameterType.RequestBody);
+
+        var response = client.Execute(request);
+
+        Console.WriteLine(response.Content);
+        return response.Content;
+    }
+
+    [JSInvokable("OnNuiEvent")]
+    public static void OnNuiEvent(string name, string data)
+    {
+        Console.WriteLine("Got event " + name);
+        if (!_events.ContainsKey(name)) return;
+
+        foreach (Action<string> callback in _events[name])
+            callback.Invoke(data);
+    }
+
+    [JSInvokable("SetParentResourceName")]
+    public static void SetParentResourceName(string name)
+    {
+        ParentResourceName = name;
+    }
+
+    public static void AddEventHandler(string name, Action<string> callback)
+    {
+        if (!_events.ContainsKey(name))
+            _events[name] = new List<Action<string>>();
+
+        Console.WriteLine("Adding event handler for " + name);
+
+        _events[name].Add(callback);
+    }
+}
